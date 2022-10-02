5d79725267b8d6a5ee82a4304eca4812651d33bd
Poroto Kekana <archibaldkekana@yahoo.com>
2022-10-02 23:23:16 +0200

    create model api

--- client/apps/inputApi.js
+++ client/apps/inputApi.js
@@ -1,5 +1,17 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('disease', () => ({
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('disease', () => ({
 
-    }))
+//     }))
+// })
+
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        uploaded_image = reader.result;
+        document.querySelector('#display_image').style.backgroundImage = `url(${uploaded_image})`
+    })
+    reader.readAsDataURL(this.files[0]);
 })
